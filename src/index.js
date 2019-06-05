f9a825dbbf7e1ab6a4682ab99b279be6e8c4b48a
Jonathan Potter <jonathan.potter.business@gmail.com>
2019-06-05 12:11:11 -0700

    adds image

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+   // Add the image to our existing div.
+   const myIcon = new Image();
+   myIcon.src = icon;
+
+   element.appendChild(myIcon);
 
   return element;
 }
